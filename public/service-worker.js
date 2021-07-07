cb7812a289c64a7a4c8223baa05ad05330180e47
ForestW70 <hexaforest@gmail.com>
2021-07-07 16:21:27 -0400

    changed icon to actually be a square (....), updates service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,64 +1,73 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/index.js',
-    '/styles.css',
-    '/assets/images/icons/icon-192x192.png',
-    '/assets/images/icons/icon-512x512.png',
-    "/manifest.webmanifest",
-    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
-  ];
-  
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
-  
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
+  '/',
+  '/index.html',
+  '/assets/js/index.js',
+  '/assets/css/styles.css',
+  '/assets/images/icons/icon-192x192.png',
+  '/manifest.webmanifest',
+  'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+];
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(PRECACHE).then(cache => {
+      cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(name => {
+            if (name !== PRECACHE && name !== RUNTIME) {
+              console.log("deciding fate of old data...", name);
+              return caches.delete(name);
+            }
+          })
+        );
+      })
+  );
+  self.clients.claim();
+});
+
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/api')) {
+    event.respondWith(
+      caches.open(RUNTIME).then(cache => {
+        return fetch(event.request)
+        .then(res => {
+          if (res.status === 200) {
+            cache.put(event.request.url, res.clone());
           }
-  
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
+          return res;
         })
-      );
-    }
-  });
-  
+        .catch(err => {
+          console.log(err);
+          return cache.match(event.request) || {};
+        });
+      }).catch(err => console.log(err))
+    );
+  }
+
+  event.respondWith(
+    fetch(event.request).catch( () => {
+      return caches.match(event.request).then(res => {
+        if (res) {
+          return res;
+        } 
+      })
+    })
+  )
+});
+
+
